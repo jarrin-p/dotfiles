568149377d7bb7aa521860ea93658bf20c85a03d
Jarrin Peters <jarrin.scott@icloud.com>
2022-05-23 08:47:36 -0400

    fixed remap... whoops

--- managed/.config/nvim/requires/Remaps.lua
+++ managed/.config/nvim/requires/Remaps.lua
@@ -52,8 +52,8 @@ nnoremap('<leader>T', ':NERDTreeToggleVCS<enter>:set rnu<enter>')  -- at vcs top
 nnoremap('<leader>g', ':tab G<enter>')
 
 -- fuzzy finding
-nnoremap('<leader>ff', "lua require('telescope.builtin').find_files()")
-nnoremap('<leader>fg', "lua require('telescope.builtin').live_grep()")
+nnoremap('<leader>ff', ":lua require('telescope.builtin').find_files()<enter>")
+nnoremap('<leader>fg', ":lua require('telescope.builtin').live_grep()<enter>")
 -- nnoremap <leader>ff <cmd>lua require('telescope.builtin').find_files()<cr>
 -- nnoremap <leader>fg <cmd>lua require('telescope.builtin').live_grep()<cr>
 -- nnoremap <leader>fb <cmd>lua require('telescope.builtin').buffers()<cr>
