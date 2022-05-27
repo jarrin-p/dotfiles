1bce5c7486baf384a756058a5b74e45083d7b8b9
jarrin-p <jarrin.scott@icloud.com>
2022-05-27 09:16:58 -0400

    refactored `Statusline.lua`
    reduced redundancy
    
    touchup
    
    broke down larger function into smaller ones that will be reused
    elsewhere

--- managed/.config/nvim/requires/Statusline.lua
+++ managed/.config/nvim/requires/Statusline.lua
@@ -2,88 +2,135 @@ require 'Global'
 require 'AutoCmdClass'
 
 -- status line modifications
-local bl, br = '«', '»' -- so i don't need to remember shortcuts
+local bl = '«'
 local te = '->'
-local enter_sym = '⏎'
+-- local br = '»'
+-- local enter_sym = '⏎'
 -- local te = '⋯'
 
--- some custom color groups for the status line
-local hl = Vim.api.nvim_set_hl
-hl(0, 'SLBracket', { bold = 0, underline = 1, ctermfg = 8, sp = Colors.h_split_underline })
-hl(0, 'SLItem', { underline = 1, ctermfg = 121, sp = Colors.h_split_underline })
-hl(0, 'SLDir', { underline = 1, italic = 1, ctermfg = 3, sp = Colors.h_split_underline })
-hl(0, 'SLFilePath', { italic = 1, underline = 1, ctermfg = 7, sp = Colors.h_split_underline })
-hl(0, 'SLFileHeader', { italic = 0, underline = 1, ctermfg = 11, sp = Colors.h_split_underline })
-hl(0, 'SLModified', { italic = 0, underline = 1, ctermfg = 9, sp = Colors.h_split_underline })
--- hl(0, 'SLMatchUL', { underline = 1, ctermfg = Colors.h_split_underline, sp = Colors.h_split_underline })
+--- nvim highlight group wrapper that allows easier inline status text formatting.
+-- additionally, has defaults specified to keep the status line uniform
+SLColorgroup = {
+    name = 'Not set',
+    scope = 0,
+    options = {
+        underline = 1, -- underline needs to be enabled for custom underline color.
+        sp = Colors.h_split_underline -- default for the underline color.
+    },
 
--- functions for easily changing colors in statusline
-local function bracket(text_to_color) return ('%#SLBracket#' .. (text_to_color or '')) end
-local function header(text_to_color) return ('%#SLFileHeader#' .. (text_to_color or '')) end
-local function directory(text_to_color) return ('%#SLDir#' .. (text_to_color or '')) end
-local function sl_item(text_to_color) return ('%#SLItem#' .. (text_to_color or '')) end
-local function mod(text_to_color) return ('%#SLModified#' .. (text_to_color or '')) end
+    --- create new statusline colorgroup object. attempting to manage statusline color groups
+    -- so its behavior can be more easily updated.
+    new = function(self, arg_table)
+        self.__index = self
+        local obj = {}
+        setmetatable(obj, self)
 
--- work in progress status line
-function GetGitRelativeDir()
-    if Vim.api.nvim_get_option_value('filetype', {}) == 'help' then
-        return header'Help'
+        for key, val in pairs(arg_table) do
+            if key == 'options' then
+                -- if it's the options table, loop through to add to the table instead of
+                -- overwriting from the new table. this preserves default settings.
+                for opts_key, opts_val in pairs(val) do
+                    obj.options[opts_key] = opts_val
+                end
 
-    elseif Vim.api.nvim_get_option_value('filetype', {}) == 'qf' then
-        return header'Quick Fix || Location List'
-
-    elseif Vim.api.nvim_get_option_value('filetype', {}) == 'fugitive' then
-        return header'Fugitive ' .. bracket(bl) .. directory' Git'
-
-    elseif Vim.api.nvim_get_option_value('filetype', {}) == 'gitcommit' then
-        return header'Commit ' .. bracket(bl) .. directory' Fugitive ' .. bracket(bl) .. directory' Git'
-
-    elseif Vim.api.nvim_get_option_value('filetype', {}) == 'git' then
-        return header'Branch ' .. bracket(bl) .. directory' Fugitive ' .. bracket(bl) .. directory' Git'
-
-    elseif Vim.api.nvim_get_option_value('filetype', {}) == 'nerdtree' then
-        return header'↟NERDTree'
-
-    elseif Vim.fn.FugitiveIsGitDir() == 1 then
-        -- use fugitive function FugitiveWorkTree() to get directory of git repo
-        local abs_file_path = {}
-        for match in Vim.fn.expand('%:p'):sub(1):gmatch('/[^/]*') do table.insert(abs_file_path, (match:gsub('/', ''))) end
-
-        local working_tree = Vim.fn.FugitiveWorkTree()
-        local _, last_index = Vim.fn.FugitiveWorkTree():find('.*/')
-        working_tree = (Vim.fn.FugitiveWorkTree():sub(last_index):gsub('/', ''))
-
-        local index_of_dir = 1
-        for i, item in ipairs(abs_file_path) do if item == working_tree then index_of_dir = i end end
-
-        local git_root_rel_path = {}
-        for i = #abs_file_path, index_of_dir, -1 do
-            table.insert(git_root_rel_path, abs_file_path[i])
+            else
+                obj[key] = val
+            end
         end
+        Vim.api.nvim_set_hl(obj.scope, obj.name, obj.options)
+        return obj
+    end,
 
-        local status = ''
-        while (#git_root_rel_path > 1) do
-            status = directory(table.remove(git_root_rel_path)) .. status
-            status =  ' ' .. bracket(bl) .. ' ' .. status
+    --- returns the string to use the color group in the status line.
+    -- @param text_to_color [optional] for code readability, to "pseudo" wrap the group of characters to be colored.
+    set = function(self, text_to_color)
+        text_to_color = text_to_color or ''
+        return '%#' .. self.name .. '#' .. text_to_color
+    end,
+}
 
-            -- set the point where truncation occurs on the list
-            if #git_root_rel_path == 7 then status = ' ' .. bracket(bl) .. directory' ...%< ' .. status end
-        end
-        status = header(table.remove(git_root_rel_path)) .. mod(AddSymbolIfSet('modified', '+')) .. status
-        return status
-    else
-        return header(Vim.fn.expand("%:p"))
+-- create custom color groups for the status line. assigning them to variables
+-- allows use of a `set` helper function so a redundant helper doesn't need to be
+-- manually declared.
+local bracket = SLColorgroup:new{ name = 'SLBracket', options = { bold = 0, ctermfg = 8 } }
+local sl_item = SLColorgroup:new{ name = 'SLItem', options = { ctermfg = 121 } }
+local directory = SLColorgroup:new{ name = 'SLDir', options = { italic = 1, ctermfg = 3 } }
+local header = SLColorgroup:new{ name = 'SLFileHeader', options = { italic = 0, ctermfg = 11 } }
+local mod = SLColorgroup:new{ name = 'SLModified', options = { italic = 0, ctermfg = 9 } }
+
+function GetFullPathAsTable()
+    local abs_file_path = {}
+    for match in Vim.fn.expand('%:p'):sub(1):gmatch('/[^/]*') do
+        table.insert(abs_file_path, (match:gsub('/', '')))
+    end
+    return abs_file_path
+end
+
+function LinearSearch(table_to_search, item_to_find)
+    for i, item in ipairs(table_to_search) do
+        if item == item_to_find then return i end
     end
 end
 
+function MakePath()
+    local file_type = Vim.api.nvim_get_option_value('filetype', {})
+    if file_type == 'help' then
+        return header:set'Help'
+
+    elseif file_type == 'qf' then
+        return header:set'Quick Fix || Location List'
+
+    elseif file_type == 'fugitive' then
+        return header:set'Fugitive ' .. bracket:set(bl) .. directory:set' Git'
+
+    elseif file_type == 'gitcommit' then
+        return header:set'Commit ' .. bracket:set(bl) .. directory:set' Fugitive ' .. bracket:set(bl) .. directory:set' Git'
+
+    elseif file_type == 'git' then
+        return header:set'Branch ' .. bracket:set(bl) .. directory:set' Fugitive ' .. bracket:set(bl) .. directory:set' Git'
+
+    elseif file_type == 'nerdtree' then
+        return header:set'↟NERDTree'
+
+    elseif Vim.fn.FugitiveIsGitDir() == 1 then
+        local abs_file_path = GetFullPathAsTable()
+
+        local _, last_index = Vim.fn.FugitiveWorkTree():find('.*/')
+        local index_of_dir = LinearSearch(abs_file_path, (Vim.fn.FugitiveWorkTree():sub(last_index):gsub('/', '')) )
+
+        -- iterate backwards to build the path to git dir
+        local git_root_rel_path = {}
+        for i = #abs_file_path, index_of_dir, -1 do table.insert(git_root_rel_path, abs_file_path[i]) end
+
+        -- while there's more than one entry left to add to the path that will be displayed
+        local status = ''
+        while (#git_root_rel_path > 1) do
+            -- pop the next item to be displayed in the path from the stack and add a bracket
+            status = directory:set(table.remove(git_root_rel_path)) .. status
+            status =  ' ' .. bracket:set(bl) .. ' ' .. status
+
+            -- set the point where truncation occurs on the list
+            if #git_root_rel_path == 7 then status = ' ' .. bracket:set(bl) .. directory:set' ...%< ' .. status end
+        end
+
+        -- the `open` file itself is the last item in the table to be popped.
+        status = header:set(table.remove(git_root_rel_path)) .. mod:set(AddSymbolIfSet('modified', '+')) .. status
+        return status
+    else
+        return header:set(Vim.fn.expand("%:p"))
+    end
+end
+
+--- uses fugitive to check if in a git directory, and if it is, return the head.
 function GetBranch()
     if Vim.fn.FugitiveIsGitDir() == 1 then
-        return sl_item("⤤ " .. Vim.fn.FugitiveHead()) .. bracket(" " .. te) .. ' '
+        return sl_item:set("⤤ " .. Vim.fn.FugitiveHead()) .. bracket:set(" " .. te) .. ' '
     else
         return ''
     end
 end
 
+--- checks if a boolean option is true, then adds a user defined symbol if it is.
 function AddSymbolIfSet(option, symbol_to_use)
     if (Vim.api.nvim_get_option_value(option, {}) == true) then
         return symbol_to_use
@@ -94,10 +141,10 @@ end
 
 function MakeStatusLine()
     -- lhs padding, also declaration for easier adjusting
-    local sl = header'  '
+    local sl = header:set'  '
 
     -- lhs
-    sl = sl .. GetGitRelativeDir()
+    sl = sl .. MakePath()
 
     -- where to truncate and where the statusline splits
     sl = sl .. "%="
@@ -107,8 +154,8 @@ function MakeStatusLine()
     sl = sl .. '        ' -- added 8 spaces of padding for when the status line is long
     sl = sl .. '        ' -- added 8 spaces of padding for when the status line is long
     sl = sl .. GetBranch()
-    sl = sl .. sl_item"buf %n" -- buffer id
-    sl = sl .. header'  ' -- rhs padding
+    sl = sl .. sl_item:set"buf %n" -- buffer id
+    sl = sl .. header:set'  ' -- rhs padding
 
     -- set based on the string
     SetWinLocal.statusline = sl
