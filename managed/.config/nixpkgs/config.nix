2d4ce3c94e5cadd82dab2c0b3141548cf931d675
jarrin-p <jarrin.scott@icloud.com>
2022-06-28 06:36:59 -0400

    created `confix.nix` for carrying around installs.

---
+++ managed/.config/nixpkgs/config.nix
@@ -0,0 +1,31 @@
+{
+  packageOverrides = pkgs: with pkgs; {
+    myPackages = pkgs.buildEnv {
+      name = "my-packages";
+      paths = [
+        bear
+        cargo
+        curl
+        ffmpeg
+        fzf
+        gh
+        git
+        gradle
+        jq
+        luarocks
+        rename
+        pkcs11helper
+        rtorrent
+        ranger
+        ripgrep
+        stow
+        terraform
+        youtube-dl
+        zsh-vi-mode
+        zsh-z
+      ];
+      pathsToLink = [ "/share" "/share/man" "/share/doc" "/bin" ];
+      extraOutputsToInstall = [ "man" "doc" ];
+    };
+  };
+}
--- restow.sh
+++ restow.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
+echo 'the bug is just a warning when using this version of stow'
+echo "running: 'stow -R managed -t ../../'"
 stow -R managed -t ../../
+echo 'restowed'
