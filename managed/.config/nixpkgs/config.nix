810f293a5d519c1f2442acee8c97c369a2d4e510
Jarrin Peters <jarrin.scott@icloud.com>
2022-07-01 19:19:28 -0400

    added more nix pkg attributes.

--- managed/.config/nixpkgs/config.nix
+++ managed/.config/nixpkgs/config.nix
@@ -1,34 +1,41 @@
 {
-  packageOverrides = pkgs: with pkgs; {
-    myPackages = pkgs.buildEnv {
-      name = "my-packages";
-      paths = [
-        bear
-        black
-        cargo
-        cmake
-        curl
-        ffmpeg
-        fzf
-        gh
-        git
-        gradle
-        jq
-        lua
-        luarocks
-        rename
-        pkcs11helper
-        rtorrent
-        ranger
-        ripgrep
-        stow
-        terraform
-        youtube-dl
-        zsh-vi-mode
-        zsh-z
-      ];
-      pathsToLink = [ "/share" "/share/man" "/share/doc" "/bin" ];
-      extraOutputsToInstall = [ "man" "doc" ];
+    packageOverrides = pkgs: with pkgs; {
+        luaEnv = lua5_3.withPackages (ps: with ps; [ luacheck ]);
+
+        py38 = pkgs.python38.withPackages (ps: with ps; [
+                pandas
+                requests
+                psycopg2
+                boto3
+        ]);
+
+        myPackages = pkgs.buildEnv {
+            name = "my-packages";
+            paths = [
+                bear
+                    black
+                    cargo
+                    cmake
+                    curl
+                    ffmpeg
+                    fzf
+                    gh
+                    git
+                    gradle
+                    jq
+                    rename
+                    pkcs11helper
+                    rtorrent
+                    ranger
+                    ripgrep
+                    stow
+                    terraform
+                    youtube-dl
+                    zsh-vi-mode
+                    zsh-z
+                    ];
+            pathsToLink = [ "/share" "/share/man" "/share/doc" "/bin" ];
+            extraOutputsToInstall = [ "man" "doc" ];
+        };
     };
-  };
 }
