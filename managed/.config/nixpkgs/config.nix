f772e1f3196f0bfcf4994f6b7dd88da72fe12e9f
jarrin-p <jarrin.scott@icloud.com>
2022-07-09 09:15:21 -0400

    lsp format on `config.nix`.

--- managed/.config/nixpkgs/config.nix
+++ managed/.config/nixpkgs/config.nix
@@ -1,42 +1,50 @@
 {
-    packageOverrides = pkgs: with pkgs; {
-        luaEnv = lua5_3.withPackages (ps: with ps; [ luacheck ]);
-
-        py38 = pkgs.python38.withPackages (ps: with ps; [
-                pandas
-                requests
-                psycopg2
-                boto3
-                virtualenv
-        ]);
-
-        myPackages = pkgs.buildEnv {
-            name = "my-packages";
-            paths = [
-                bear
-                    black
-                    cargo
-                    cmake
-                    curl
-                    ffmpeg
-                    fzf
-                    gh
-                    git
-                    gradle
-                    jq
-                    rename
-                    pkcs11helper
-                    rtorrent
-                    ranger
-                    ripgrep
-                    stow
-                    terraform
-                    youtube-dl
-                    zsh-vi-mode
-                    zsh-z
-                    ];
-            pathsToLink = [ "/share" "/share/man" "/share/doc" "/bin" ];
-            extraOutputsToInstall = [ "man" "doc" ];
-        };
+  packageOverrides = pkgs: with pkgs; {
+    # don't need this on every computer.
+    transmission = pkgs.buildEnv {
+      name = "transmission";
+      paths = [
+        transmission
+      ];
     };
+
+    luaEnv = lua5_3.withPackages (ps: with ps; [ luacheck ]);
+
+    py38 = pkgs.python38.withPackages (ps: with ps; [
+      pandas
+      requests
+      psycopg2
+      boto3
+      virtualenv
+    ]);
+
+    mainEnv = pkgs.buildEnv {
+      name = "mainEnv";
+      paths = [
+        bear
+        black
+        cargo
+        cmake
+        curl
+        ffmpeg
+        fzf
+        gh
+        git
+        gradle
+        jq
+        rename
+        pkcs11helper
+        rtorrent
+        ranger
+        ripgrep
+        stow
+        terraform
+        youtube-dl
+        zsh-vi-mode
+        zsh-z
+      ];
+      pathsToLink = [ "/share" "/share/man" "/share/doc" "/bin" ];
+      extraOutputsToInstall = [ "man" "doc" ];
+    };
+  };
 }
