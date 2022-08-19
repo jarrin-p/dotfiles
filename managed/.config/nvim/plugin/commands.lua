7e1b07fd3f028c40e5b03a72ed8a8a3714e87576
Jarrin Peters <jarrin.scott@icloud.com>
2022-08-19 11:45:50 -0400

    better formatting, added command alias.

--- managed/.config/nvim/plugin/commands.lua
+++ managed/.config/nvim/plugin/commands.lua
@@ -2,14 +2,17 @@
 --- @file `commands.lua`
 -- runs `spotlessApply` at the top level of the git repository.
 Exec(
-    [[ command SA !cd $(git rev-parse --show-toplevel); gradle spotlessApply ]],
+    [[ command! SA !cd $(git rev-parse --show-toplevel); gradle spotlessApply ]],
         false
 )
 
 -- changes current directory to the root of the git repository.
-Exec([[ command GT execute 'cd' fnameescape(FugitiveWorkTree())]], false)
+Exec([[ command! GT execute 'cd' fnameescape(FugitiveWorkTree())]], false)
 
 -- loads the Sessiom.vim from the root of the git directory if in one.
-Exec([[ command LG lua LoadGitSession()]], false)
+Exec([[ command! LG lua LoadGitSession()]], false)
+
+-- alias for opening quickfix list to a new tab.
+Exec([[ command! CO tabnew | copen | only]], false)
 
 -- vim: fdm=marker foldlevel=0
