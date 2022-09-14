8c8265e7a0a7afc87d30cc66d456a5763e0bf761
Jarrin Peters <jarrin.scott@icloud.com>
2022-09-14 09:28:59 -0400

    added goto_next() and prev remaps. sty reformat.

--- managed/.config/nvim/plugin/p_lspconfig.lua
+++ managed/.config/nvim/plugin/p_lspconfig.lua
@@ -1,30 +1,28 @@
 require('nvim-lsp-installer').setup {}
 local servers = {
-    'pyright', 'jdtls', 'sumneko_lua', 'terraformls', 'bashls', 'remark_ls',
-    'rnix', 'vimls', 'tsserver', 'groovyls',
+    'pyright',
+    'jdtls',
+    'sumneko_lua',
+    'terraformls',
+    'bashls',
+    'remark_ls',
+    'rnix',
+    'vimls',
+    'tsserver',
+    'groovyls',
 }
 
-local capabilities = require('cmp_nvim_lsp').update_capabilities(
-    vim.lsp.protocol.make_client_capabilities()
-)
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 local r = require('lspconfig')
 for _, s in pairs(servers) do
     if s == 'sumneko_lua' then
         r[s].setup {
             capabilities = capabilities,
-            settings = {
-                Lua = {
-                    version = 'LuaJIT',
-                    diagnostics = { globals = { 'vim' } },
-                },
-            },
+            settings = { Lua = { version = 'LuaJIT', diagnostics = { globals = { 'vim' } } } },
         }
     elseif s == 'bashls' then
-        r[s].setup {
-            capabilities = capabilities,
-            filetypes = { 'sh', 'bash', 'zsh' },
-        }
+        r[s].setup { capabilities = capabilities, filetypes = { 'sh', 'bash', 'zsh' } }
     elseif s == 'jdtls' then
         r[s].setup { capabilities = capabilities, use_lombok_agent = true }
     else
@@ -41,3 +39,5 @@ nnoremap('gc', ':lua vim.lsp.buf.code_action()<enter>')
 nnoremap('g=', ':lua vim.lsp.buf.formatting()<enter>')
 nnoremap('gw', ':lua vim.lsp.buf.workspace_symbol()<enter>')
 nnoremap('gs', ':lua vim.lsp.buf.signature_help()<enter>')
+nnoremap('gj', ':lua vim.diagnostic.goto_next()<enter>')
+nnoremap('gk', ':lua vim.diagnostic.goto_prev()<enter>')
