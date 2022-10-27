a63daed7bab202e143a7756b55ac309083320900
jarrin-p <jarrin.scott@icloud.com>
2022-10-27 09:17:44 -0400

    added menu wrapper.

---
+++ managed/.config/nvim/plugin/menus.lua
@@ -0,0 +1,26 @@
+--- @file `files.lua`
+--- @author jarrin-p
+-- start by cleaning the menus out.
+vim.cmd('aunmenu *')
+
+--- @class menu_opts
+--- @field name string name of the menu item.
+--- @field map string keymap to provide for the map.
+--- @field noremap? boolean default `false`. whether or not to search for remaps.
+--- @field mode? string what modes this menu will be valid for. see `:h map-modes`.
+
+--- @param opts menu_opts settings to pass to mapping. hover `opts` for details.
+local function AddMenuItem(opts)
+    if not opts.map or not opts.name then
+        print('`opts.map` not provided. aborting function.')
+        return
+    end
+    local mode = opts.mode or ''
+    local noremap = (opts.noremap and 'nno') or ''
+
+    local cmd = table.concat({ mode .. noremap .. 'menu', opts.name, opts.map }, ' ')
+    vim.cmd(cmd)
+end
+
+AddMenuItem { name = 'test.test', map = ':w<enter>' }
+AddMenuItem { name = 'test.anothertest', map = ':w<enter>' }
