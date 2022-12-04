a02e3b0c97a0fa0b725097d9effbb4ec0eb69b0d
jarrin-p <jarrin.scott@icloud.com>
2022-12-03 21:31:07 -0500

    cleaned `colorscheme.lua`.

--- managed/.config/nvim/plugin/colorscheme.lua
+++ managed/.config/nvim/plugin/colorscheme.lua
@@ -18,7 +18,7 @@ Colors = {
     term = { cyan = 81, blue_dark = 8, red = 9 },
 
     gui = {
-        gray = '#859289',
+        comment_fg = GetColorschemeAsHex("Comment", "foreground"),
         gray_lighter = '#515354',
         green = '#a7c080',
         green_bright = '#a9dd9d',
@@ -26,102 +26,9 @@ Colors = {
         wood_dark = '#f0aa8a',
         wood_light = '#ffebc3',
         statusline_bg = '#374247',
-        green_dark = '#242b30',
-        sl_filename = '#a7c080',
-        -- green_dark = '#1d2226', -- good
+        cursor_fg = GetColorschemeAsHex("Cursor", "foreground"),
+        boolean_fg = GetColorschemeAsHex("Boolean", "foreground"),
     },
 } -- }}}
 
---- helper functions {{{
-
---- @see `:h synID()` and its relatives for details.
---- TODO add color scheme inspector for concealed items.
-local what_table = {
-    'name',
-    'fg',
-    'bg',
-    'font',
-    'sp',
-    'fg#',
-    'bg#',
-    'sp#',
-    'bold',
-    'italic',
-    'reverse',
-    'inverse',
-    'standout',
-    'underline',
-    'underlineline',
-    'undercurl',
-    'underdot',
-    'underdash',
-    'strikethrough',
-}
-
---- gets the `what` from synID(). will also follow links to return the real value of the field.
---- @param args.line, args.col (integer) line and col number of what is to be inspected, respectively.
---- @param args.trans (float) the transparency of the item
---- @param args.mode (string) 'gui', 'cterm', or 'term' for specifying which type of field you want.
-function GetColorschemeField(args)
-    if not args or not args.what then
-        print('need to provide keyword argument `what = ...`')
-        return
-    end
-    args.line = args.line or vim.fn.line('.')
-    args.col = args.col or vim.fn.col('.')
-    args.trans = args.trans or 1
-    args.mode = args.mode or nil
-
-    local synID = vim.fn.synID(args.line, args.col, args.trans)
-    local synTrans = vim.fn.synIDtrans(synID)
-    local synIDattr = vim.fn.synIDattr(synTrans, args.what)
-    if args.toOut then print(synIDattr) end
-    return synIDattr
-end
-
-function InspectColorscheme(what)
-    what = what or what_table
-    if type(what) ~= 'table' then
-        print('argument is not a table. exiting function.')
-        return
-    end
-
-    for _, value in ipairs(what) do print(value .. ': ' .. GetColorschemeField { what = value }) end
-end -- }}}
-
--- color scheme changes {{{
--- vim.api.nvim_set_hl(0, 'CursorLine', { underline = 0, sp = Colors.gui.gray, bg = Colors.gui.green_dark })
--- vim.api.nvim_set_hl(0, 'CursorLine', { underline = 0, sp = Colors.gui.gray })
--- vim.api.nvim_set_hl(0, 'CursorLineNr', { underline = 0, sp = Colors.gui.gray })
--- vim.api.nvim_set_hl(0, 'DiagnosticUnderlineError', { undercurl = 1, sp = Colors.gui.red })
--- vim.api.nvim_set_hl(0, 'DiagnosticUnderlineHint', { undercurl = 1, sp = Colors.gui.gray })
--- vim.api.nvim_set_hl(0, 'DiagnosticUnderlineInfo', { undercurl = 1, sp = Colors.gui.wood_dark })
--- vim.api.nvim_set_hl(0, 'DiagnosticUnderlineWarn', { undercurl = 1, sp = Colors.gui.wood_light })
--- vim.api.nvim_set_hl(0, 'DiffAdd', { fg = Colors.gui.green_bright, ctermfg = 6, bold = 1 })
--- vim.api.nvim_set_hl(0, 'DiffChange', { fg = Colors.gui.wood_light, ctermfg = 12, italic = 1 })
--- vim.api.nvim_set_hl(0, 'DiffDelete', { fg = Colors.gui.red, ctermfg = 1, bold = 1 })
--- vim.api.nvim_set_hl(0, 'DiffText', { fg = Colors.gui.wood_dark, ctermfg = 11 })
--- vim.api.nvim_set_hl(0, 'EndOfBuffer', { fg = 'bg', ctermfg = 11 })
--- vim.api.nvim_set_hl(0, 'Error', { undercurl = 1, sp = 'red' })
--- vim.api.nvim_set_hl(0, 'Folded', { fg = Colors.gui.gray, ctermfg = Colors.term.blue_dark, italic = 1 })
--- vim.api.nvim_set_hl(0, 'MatchParen', { fg = Colors.gui.red, bold = 1, italic = 1 })
--- vim.api.nvim_set_hl(0, 'NonText', { fg = Colors.gui.gray, ctermfg = Colors.term.blue_dark })
--- vim.api.nvim_set_hl(0, 'Search', { bold = 1, italic = 1, ctermfg = Colors.cyan, fg = Colors.gui.wood_light })
--- vim.api.nvim_set_hl(0, 'SignColumn', {})
--- vim.api.nvim_set_hl(0, 'SpecialKey', { italic = 1, fg = Colors.gui.red })
--- vim.api.nvim_set_hl(0, 'SpellBad', { undercurl = 1, sp = Colors.gui.red })
--- vim.api.nvim_set_hl(0, 'SpellCap', { ctermbg = Colors.term.blue_dark, undercurl = 1, sp = Colors.gui.gray })
-vim.api.nvim_set_hl(0, 'StatusLine', {
-    ctermbg = Colors.term.blue_dark,
-    underline = 1,
-    sp = Colors.gui.statusline_bg,
-    bg = Colors.gui.statusline_bg,
-})
-vim.api.nvim_set_hl(0, 'StatusLineNC', { ctermbg = Colors.term.blue_dark, bg = Colors.gui.statusline_bg })
--- vim.api.nvim_set_hl(0, 'Todo', { bold = 1, fg = Colors.gui.gray })
--- vim.api.nvim_set_hl(0, 'VertSplit', { ctermfg = 0, fg = Colors.gui.gray_lighter })
--- vim.api.nvim_set_hl(0, 'Whitespace', { fg = Colors.gui.gray, ctermfg = Colors.term.blue_dark }) -- }}}
---
--- vim.api.nvim_set_hl(0, 'NormalPopupTerm', {})
--- vim.api.nvim_set_hl(0, 'BorderPopupTerm', { fg = Colors.gui.gray_lighter })
--- vim.api.nvim_set_hl(0, guifg= guibg=#4a555b
+vim.api.nvim_set_hl(0, 'TablineSel', { fg = Colors.gui.cursor_fg, bg = Colors.gui.boolean_fg })
