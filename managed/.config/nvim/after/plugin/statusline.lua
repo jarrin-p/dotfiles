b5350738307f349ed273c3436736207c55823c2c
jarrin-p <jarrin.scott@icloud.com>
2022-12-09 13:11:46 -0500

    removed unused functions. redeclared some repeat items as objects.

--- managed/.config/nvim/after/plugin/statusline.lua
+++ managed/.config/nvim/after/plugin/statusline.lua
@@ -6,7 +6,7 @@ LS = require 'luasnip'
 --- create custom color groups for the status line. assigning them to variables
 --- allows the color groups to have a `set` helper function that uses defaults.
 local bracket = InlineColorGroup:new{ name = 'SLBracket', options = { fg = Colors.gui.comment_fg } }
-local sl_item = InlineColorGroup:new{ name = 'SLItem', options = { fg = Colors.gui.boolean_fg } }
+-- local sl_item = InlineColorGroup:new{ name = 'SLItem', options = { fg = Colors.gui.boolean_fg } }
 local directory = InlineColorGroup:new{
     name = 'SLDir',
     options = { italic = 1, ctermfg = 3, fg = Colors.gui.comment_fg, bg = Colors.gui.normal_bg },
@@ -27,6 +27,11 @@ local mod = InlineColorGroup:new{
 }
 local sl_norm = InlineColorGroup:new{ name = 'SLNorm', options = { bg = Colors.gui.normal_bg } }
 
+-- specific objects that get reused.
+local header_la = header_reverse:set(Symbols.left_tr)
+local dir_git = directory:set 'Git'
+local dir_fug = directory:set 'Fugitive'
+
 --- functions {{{
 --- gets the absolute path of the currently worked on file using `expand`
 --- and splits it into a table.
@@ -46,33 +51,42 @@ function LinearSearch(table_to_search, item_to_find)
     return -1
 end
 
+--- checks if a boolean option is true, then adds a user defined symbol if it is.
+--- @param option string the vim option to check if it's true or not.
+--- @param symbol_to_use string the value to be returned if the option is true.
+--- @return string #empty if false, otherwise returns `symbol_to_use`.
+function GetIfSet(option, symbol_to_use)
+    if (vim.api.nvim_get_option_value(option, {}) == true) then return symbol_to_use end
+    return ''
+end
+
+local function getModifiedStatus() return mod:set(GetIfSet('modified', '+')) end
+
 --- build the path itself.
 function MakePath()
     local file_type = vim.api.nvim_get_option_value('filetype', {})
     local buf_type = vim.api.nvim_get_option_value('buftype', {})
 
     if buf_type == 'terminal' then
-        return header:set 'Terminal  ' .. header_reverse:set(Symbols.left_tr)
+        return header:set 'Terminal  ' .. header_la
 
     elseif file_type == 'minimap' then
-        return header:set 'Minimap  ' .. header_reverse:set(Symbols.left_tr)
+        return header:set 'Minimap  ' .. header_la
 
     elseif file_type == 'help' then
-        return header:set 'Help  ' .. header_reverse:set(Symbols.left_tr)
+        return header:set 'Help  ' .. header_la
 
     elseif file_type == 'qf' then
-        return header:set 'Quick Fix || Location List  ' .. header_reverse:set(Symbols.left_tr)
+        return header:set 'Quick Fix || Location List  ' .. header_la
 
     elseif file_type == 'fugitive' then
-        return header:set 'Fugitive  ' .. header_reverse:set(Symbols.left_tr) .. directory:set ' Git'
+        return header:set 'Fugitive  ' .. header_la .. dir_git
 
     elseif file_type == 'gitcommit' then
-        return header:set 'Commit  ' .. header_reverse:set(Symbols.left_tr) .. directory:set ' Fugitive '
-                   .. directory:set ' Git'
+        return header:set 'Commit  ' .. header_la .. dir_fug .. dir_git
 
     elseif file_type == 'git' then
-        return header:set 'Branch  ' .. header_reverse:set(Symbols.left_tr) .. directory:set ' Fugitive '
-                   .. directory:set ' Git'
+        return header:set 'Branch  ' .. header_la .. dir_fug .. dir_git
 
     elseif vim.fn.FugitiveIsGitDir() == 1 then
         local abs_file_path = GetAbsolutePathAsTable()
@@ -116,48 +130,19 @@ function ConvertTableToPathString(path_table, truncate_point, project_root_index
 
     --- the `open` file itself is the last item in the table to be popped.
     --- additionally, adds a modified symbol if ... the file has been modified ...
-    status = header:set(table.remove(path_table)) .. mod:set(AddSymbolIfSet('modified', '+')) .. header:set ' '
+    status = header:set(table.remove(path_table)) .. getModifiedStatus() .. header:set ' '
                  .. header_reverse:set(Symbols.left_tr) .. bracket:set '' .. status
     return status
 end
 
---- checks if a boolean option is true, then adds a user defined symbol if it is.
-function AddSymbolIfSet(option, symbol_to_use)
-    if (vim.api.nvim_get_option_value(option, {}) == true) then return symbol_to_use end
-    return ''
-end
-
---- finally, customize the statusline using the components we made.
---- function MakeStatusLine()
+--- definition of the statusline as a vim global function in order to reference it
+--- inside the statusline functino call natively.
 vim.g.MakeStatusLine = function()
     -- left hand side padding, also declaration for easier adjusting.
     local sl = header:set '  '
-
-    -- left hand side.
-    if LS.in_snippet() then
-        sl = sl .. 'in snippet'
-        if LS.expand_or_jumpable() then
-            if LS.expandable() then
-                sl = sl .. bracket:set ' ->' .. sl_item:set ' [expandable]' .. directory:set ' <tab>'
-            end
-            if LS.jumpable() then
-                sl = sl .. bracket:set ' ->' .. sl_item:set ' [jumpable]' .. directory:set ' <tab>'
-            end
-        end
-        if LS.choice_active() then
-            local choices = LS.get_current_choices()
-            for i, choice in ipairs(choices) do
-                if choice == '' then choices[i] = '[empty]' end
-                choices[i] = sl_item:set(choices[i])
-            end
-            sl = sl .. bracket:set ' ->' .. sl_item:set ' choice node ' .. directory:set '<c-j>' .. bracket:set ' || '
-                     .. directory:set '<c-k>' .. bracket:set ': ' .. table.concat(choices, directory:set ' | ')
-        end
-    else
-        sl = sl .. MakePath()
-    end
-
+    sl = sl .. MakePath()
     sl = sl .. sl_norm:set ''
+
     -- updates the window being worked in only.
     vim.wo.statusline = sl
     return sl
