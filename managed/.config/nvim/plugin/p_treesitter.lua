013e968ba724cf26d4c574755ab530a96b468dd3
jarrin-p <jarrin.scott@icloud.com>
2022-12-30 13:53:42 -0500

    temporarily disabling treesitter.

--- managed/.config/nvim/plugin/p_treesitter.lua
+++ managed/.config/nvim/plugin/p_treesitter.lua
@@ -1,9 +1,9 @@
-require'nvim-treesitter.configs'.setup {
-    ensure_installed = 'all', -- 'all', 'maintained', or a table of languages
-    sync_install = true,
-    highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = true,
-        disable = { 'vim', 'help' }, -- format for disabling languages.
-    },
-}
+-- require'nvim-treesitter.configs'.setup {
+--     ensure_installed = 'all', -- 'all', 'maintained', or a table of languages
+--     sync_install = true,
+--     highlight = {
+--         enable = true,
+--         additional_vim_regex_highlighting = true,
+--         disable = { 'vim', 'help' }, -- format for disabling languages.
+--     },
+-- }
