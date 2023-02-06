26dc6eb96cbcbd7a852feac4f9fb44d7ddc366e6
jarrin-p <jarrin.scott@icloud.com>
2023-02-05 21:41:38 -0500

    disabled some more arm broken treesitter stuff.

--- managed/.config/nvim/plugin/p_treesitter.lua
+++ managed/.config/nvim/plugin/p_treesitter.lua
@@ -4,6 +4,6 @@ require'nvim-treesitter.configs'.setup {
     highlight = {
         enable = true,
         additional_vim_regex_highlighting = true,
-        disable = { 'vim', 'help' }, -- format for disabling languages.
+        disable = { 'vim', 'help', 'bash', 'zsh', 'sh', 'nix' }, -- format for disabling languages.
     },
 }
