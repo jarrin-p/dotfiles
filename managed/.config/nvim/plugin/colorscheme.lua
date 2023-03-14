17ad431c26470cfa6972e756010f86232e21d85d
jarrin-p <jarrin.scott@icloud.com>
2023-03-13 23:19:44 -0400

    added opacity transition tool.

--- managed/.config/nvim/plugin/colorscheme.lua
+++ managed/.config/nvim/plugin/colorscheme.lua
@@ -1,14 +1,36 @@
 --- @author jarrin-p
 --- @file `colorscheme.lua`
 local util = require 'pack.statusline.util.util'
+local apply_opacity = require 'hex_tool'.apply_opacity_transition
 
 local fg = util.get_colorscheme_as_hex('Fg', 'foreground')
+local bg = util.get_colorscheme_as_hex('Normal', 'background')
 local darker = util.get_colorscheme_as_hex('FloatBorder', 'background')
 
-vim.api.nvim_set_hl(0, 'DiffAdd', { link = 'GreenItalic' })
-vim.api.nvim_set_hl(0, 'DiffChange', { link = 'OrangeItalic' })
-vim.api.nvim_set_hl(0, 'DiffDelete', { link = 'RedItalic' })
-vim.api.nvim_set_hl(0, 'DiffText', { link = 'YellowItalic' })
+local green = util.get_colorscheme_as_hex('Green', 'foreground')
+local red = util.get_colorscheme_as_hex('Red', 'foreground')
+local orange = util.get_colorscheme_as_hex('Orange', 'foreground')
+-- local dark_green = '#252715'
+-- local dark_red = '#251110'
+-- local dark_orange = '#25160c'
+--
+local opacity = .15
+local dark_green = apply_opacity(green, bg, opacity)
+local dark_red = apply_opacity(red, bg, opacity)
+local dark_orange = apply_opacity(orange, bg, opacity)
+
+
+--- @param front string hex color.
+--- @param behind string hex color.
+--- @return string #hex color after opacity applied.
+local function apply_opacity(front, behind)
+
+end
+
+vim.api.nvim_set_hl(0, 'DiffAdd', { bg = dark_green })
+vim.api.nvim_set_hl(0, 'DiffChange', { bg = dark_orange })
+vim.api.nvim_set_hl(0, 'DiffDelete', { bg = dark_red, fg = dark_red })
+vim.api.nvim_set_hl(0, 'DiffText', { bg = dark_orange })
 vim.api.nvim_set_hl(0, 'EndOfBuffer', { fg = 'bg' })
 vim.api.nvim_set_hl(0, 'Search', { bg = darker, underline = true, italic = true })
 vim.api.nvim_set_hl(0, 'CurSearch', { bg = darker, underline = true, italic = true, bold = true })
