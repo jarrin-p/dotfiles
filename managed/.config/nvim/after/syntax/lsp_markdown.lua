548bb24efeb96bb16f547bdf0b1d5f10cd2bd739
Jarrin Peters <jarrin.scott@icloud.com>
2023-03-29 19:23:36 -0400

    "fix" lsp markdown.

--- managed/.config/nvim/after/syntax/lsp_markdown.lua
+++ managed/.config/nvim/after/syntax/lsp_markdown.lua
@@ -1,4 +1,3 @@
 vim.cmd('syn clear markdownLink')
-vim.cmd(
-    'syn region markdownLink matchgroup=markdownLinkDelimiter start="(" end=")" contains=markdownUrl,mkJdtLink keepend contained')
-vim.cmd('syntax match mkJdtLink /jdt://.*/ containedin=markdownLink conceal')
+vim.cmd('syn region markdownLink matchgroup=markdownLinkDelimiter start="(" end=")" contains=markdownUrl,mkJdtLink keepend contained')
+vim.cmd('syntax match mkJdtLink /jdt:\\/\\/.*/ containedin=markdownLink conceal')
