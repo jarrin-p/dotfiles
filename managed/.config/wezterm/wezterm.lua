e858894255ff02202cc7b14f2c6eedc30cf9a2c0
jarrin-p <jarrin.scott@icloud.com>
2023-06-26 07:38:50 -0400

    added config file.

---
+++ managed/.config/wezterm/wezterm.lua
@@ -0,0 +1,12 @@
+local wezterm = require 'wezterm'
+
+local config = {}
+if wezterm.config_builder then
+    config = wezterm.config_builder()
+end
+
+config.color_scheme = 'Gruvbox dark, hard (base16)'
+config.font = wezterm.font('JetBrains Mono')
+-- config.font = wezterm.font('JetBrains Mono', { weight = 'Bold', italic = true })
+
+return config
