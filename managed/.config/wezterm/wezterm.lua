b8a0f5dd529d6b950a59634f6ff3847c5a870924
jarrin-p <jarrin.scott@icloud.com>
2023-06-26 08:25:00 -0400

    formatting.

--- managed/.config/wezterm/wezterm.lua
+++ managed/.config/wezterm/wezterm.lua
@@ -5,8 +5,19 @@ if wezterm.config_builder then
     config = wezterm.config_builder()
 end
 
-config.color_scheme = 'Gruvbox dark, hard (base16)'
-config.font = wezterm.font('JetBrains Mono')
--- config.font = wezterm.font('JetBrains Mono', { weight = 'Bold', italic = true })
 config.enable_tab_bar = false
+config.color_scheme = 'Gruvbox dark, hard (base16)'
+
+config.font = wezterm.font('JetBrains Mono')
+config.font_size = 13
+
+-- window settings
+config.enable_scroll_bar = true
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
+
 return config
