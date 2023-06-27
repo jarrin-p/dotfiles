5641f300bf4107d28f3e142f91c5385e02f27f9e
jarrin-p <jarrin.scott@icloud.com>
2023-06-27 07:53:46 -0400

    update colorscheme.

--- managed/.config/nvim/plugin/colorscheme.lua
+++ managed/.config/nvim/plugin/colorscheme.lua
@@ -19,22 +19,27 @@ local dark_green = apply_opacity(green, bg, opacity)
 local dark_red = apply_opacity(red, bg, opacity)
 local dark_orange = apply_opacity(orange, bg, opacity)
 
-vim.api.nvim_set_hl(0, 'DiffAdd', { bg = dark_green })
-vim.api.nvim_set_hl(0, 'DiffAdded', { link = 'DiffAdd' })
+local hl = vim.api.nvim_set_hl
+hl(0, 'DiffAdd', { bg = dark_green })
+hl(0, 'DiffAdded', { link = 'DiffAdd' })
 
-vim.api.nvim_set_hl(0, 'DiffDelete', { bg = dark_red, fg = dark_red })
-vim.api.nvim_set_hl(0, 'DiffRemoved', { bg = dark_red })
+hl(0, 'DiffDelete', { bg = dark_red, fg = dark_red })
+hl(0, 'DiffRemoved', { bg = dark_red })
 
-vim.api.nvim_set_hl(0, 'DiffChange', { bg = dark_orange })
-vim.api.nvim_set_hl(0, 'DiffText', { bg = dark_orange })
+hl(0, 'DiffChange', { bg = dark_orange })
+hl(0, 'DiffText', { bg = dark_orange })
 
-vim.api.nvim_set_hl(0, 'EndOfBuffer', { fg = 'bg' })
-vim.api.nvim_set_hl(0, 'Search', { bg = darker, underline = true, italic = true })
-vim.api.nvim_set_hl(0, 'CurSearch', { bg = darker, underline = true, italic = true, bold = true })
-vim.api.nvim_set_hl(0, 'String', { link = 'AquaItalic' })
-vim.api.nvim_set_hl(0, 'MsgArea', { fg = fg, bg = darker })
-vim.api.nvim_set_hl(0, 'TablineFill', { link = 'Normal' })
+hl(0, 'EndOfBuffer', { fg = 'bg' })
+hl(0, 'Search', { bg = darker, underline = true, italic = true })
+hl(0, 'CurSearch', { bg = darker, underline = true, italic = true, bold = true })
+hl(0, 'String', { link = 'AquaItalic' })
+hl(0, 'MsgArea', { fg = fg, bg = darker })
+hl(0, 'TablineFill', { link = 'Normal' })
 
-vim.api.nvim_set_hl(0, 'Whitespace', { fg = dark_orange })
+hl(0, 'Whitespace', { fg = dark_orange })
 
-
+hl(0, 'SignColumn', { link = 'Normal' })
+hl(0, 'CursorLineNr', { link = 'Normal'})
+hl(0, 'CursorLineSign', { link = 'Normal'})
+hl(0, 'LineNr', { link = 'Normal'})
+hl(0, 'FoldColumn', { link = 'Normal'})
