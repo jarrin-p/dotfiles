e56cae62fde9a08c8b723e2ab4d6b902400a2e89
jarrin-p <jarrin.scott@icloud.com>
2023-07-07 09:28:59 -0400

    added notes, cleaned comments.

--- managed/.config/nvim/plugin/p_treesitter.lua
+++ managed/.config/nvim/plugin/p_treesitter.lua
@@ -1,11 +1,9 @@
 require'nvim-treesitter.configs'.setup {
-    -- ensure_installed = 'all', -- 'all', 'maintained', or a table of languages
-    -- sync_install = true,
     highlight = {
         enable = true,
+        -- note: if there's an issue with treesitter parsing, try
+        -- uninstalling via nix, collecting garbage, re-installing.
+        -- disable = { 'vim' },
         additional_vim_regex_highlighting = false,
-
-        -- todo: look into why these languages are throwing errors.
-        -- disable = { 'vim', 'help', 'bash', 'zsh', 'sh', 'nix', 'dockerfile', 'make', 'markdown', 'typescript', 'sql' },
     },
 }
