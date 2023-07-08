2a100f1a593f6519a465c5b30ea1d8289c98d44b
jarrin-p <jarrin.scott@icloud.com>
2023-07-08 07:09:14 -0400

    update color specs.

---
+++ managed/.config/nvim/pack/statusline/util/color_specs.lua
@@ -0,0 +1,6 @@
+local M = {
+    fg = { hl_name = 'Fg', color_type = 'foreground' },
+    lighter = { hl_name = 'Tabline', color_type = 'background' },
+    darker = { hl_name = 'FloatBorder', color_type = 'background' },
+}
+return M
--- managed/.config/nvim/pack/statusline/util/config.lua
+++ managed/.config/nvim/pack/statusline/util/config.lua
@@ -1,12 +1,13 @@
 local cg = require 'pack.statusline.util.component'
 local util = require 'pack.statusline.util.util'
 local symbols = require 'pack.statusline.util.symbols'
+local spec = require 'pack.statusline.util.color_specs'
 
 M = {}
 
-M.fg = util.get_colorscheme_as_hex('Fg', 'foreground')
-M.lighter = util.get_colorscheme_as_hex('Tabline', 'background')
-M.darker = util.get_colorscheme_as_hex('FloatBorder', 'background')
+M.fg = util.get_colorscheme_as_hex(spec.fg.hl_name, spec.fg.color_type)
+M.lighter = util.get_colorscheme_as_hex(spec.lighter.hl_name, spec.lighter.color_type)
+M.darker = util.get_colorscheme_as_hex(spec.darker.hl_name, spec.darker.color_type)
 
 -- make sure we create the color groups before we start using them for
 -- transitions.
@@ -17,11 +18,8 @@ M.fill = cg:new{ hl_name = 'Statusline' }
 set_hl(0, 'StatuslineHeader', { fg = 'Black', bg = M.fg })
 M.header = cg:new{ hl_name = 'StatuslineHeader' }
 
-set_hl(0, 'StatuslineDirectory', {
-    italic = 1,
-    fg = util.get_colorscheme_as_hex('Comment', 'foreground'),
-    bg = M.darker,
-})
+set_hl(0, 'StatuslineDirectory',
+    { italic = 1, fg = util.get_colorscheme_as_hex('Comment', 'foreground'), bg = M.darker })
 M.directory = cg:new{ hl_name = 'StatuslineDirectory' }
 
 set_hl(0, 'StatuslineFtInfo', { fg = M.fg, bg = M.lighter })
@@ -37,20 +35,14 @@ M.transitions = {
     fill_to_ft_info = M.ft_info:get_transition_to(M.fill, 'background', symbols.right_tr):get_value(),
 }
 
-M.buffer_types = {
-    terminal = M.header:set '  Terminal  ' .. M.transitions.header_to_dir,
-}
+M.buffer_types = { terminal = M.header:set '  Terminal  ' .. M.transitions.header_to_dir }
 M.file_types = {
     minimap = M.header:set '  Minimap  ' .. M.transitions.header_to_dir,
     help = M.header:set '  Help  ' .. M.transitions.header_to_dir,
-    qf = M.header:set '  Quick Fix || Location List  '
-        .. M.transitions.header_to_dir,
-    fugitive = M.header:set '  Fugitive  ' .. M.transitions.header_to_dir
-        .. M.dir_git,
-    gitcommit = M.header:set '  Commit  ' .. M.transitions.header_to_dir
-        .. M.dir_fug .. M.dir_git,
-    git = M.header:set '  Branch  ' .. M.transitions.header_to_dir .. M.dir_fug
-        .. M.dir_git,
+    qf = M.header:set '  Quick Fix || Location List  ' .. M.transitions.header_to_dir,
+    fugitive = M.header:set '  Fugitive  ' .. M.transitions.header_to_dir .. M.dir_git,
+    gitcommit = M.header:set '  Commit  ' .. M.transitions.header_to_dir .. M.dir_fug .. M.dir_git,
+    git = M.header:set '  Branch  ' .. M.transitions.header_to_dir .. M.dir_fug .. M.dir_git,
 }
 
 return M
