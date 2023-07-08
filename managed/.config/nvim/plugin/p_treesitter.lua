457707b2562123658969b23fb9c7e94c58cf9ce6
jarrin-p <jarrin.scott@icloud.com>
2023-07-08 07:09:27 -0400

    added treesitter incremental seleciton.

--- managed/.config/nvim/plugin/p_treesitter.lua
+++ managed/.config/nvim/plugin/p_treesitter.lua
@@ -6,4 +6,13 @@ require'nvim-treesitter.configs'.setup {
         -- disable = { 'vim' },
         additional_vim_regex_highlighting = false,
     },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "gnn",
+        node_incremental = "grn",
+        scope_incremental = "grc",
+        node_decremental = "grm",
+      },
+    },
 }
