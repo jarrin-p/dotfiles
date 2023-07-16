d359892ef40b75a673c92ba1a0a5043c74adbecd
jarrin-p <jarrin.scott@icloud.com>
2023-07-16 15:29:02 -0400

    rename for more obvious use case.

---
+++ managed/.config/nvim/nix_hook.lua
@@ -0,0 +1,11 @@
+--- using `setup.lua` instead of `init.lua` because init is on the lua search path already,
+--- however it is desired for this to be called before nvim calls anything else.
+require "os"
+
+--- compile all fennel into lua first using fennel application (should be installed via nixpkgs).
+--- this saves overhead of having to load fennel compiler into neovim each startup.
+local aot_compile_filename = "fennel_aot_compile.fnl"
+local command = "fennel " .. os.getenv("HOME") .. "/.config/nvim/" .. aot_compile_filename
+os.execute(command)
+require "util"
+require "plugins"
