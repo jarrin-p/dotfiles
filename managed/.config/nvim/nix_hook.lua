6d31f9d83c7a5b20355d5ea5ab42211d0d552379
Jarrin Peters <jarrin.scott@icloud.com>
2023-07-28 05:56:34 -0400

    pause aot compile.

--- managed/.config/nvim/nix_hook.lua
+++ managed/.config/nvim/nix_hook.lua
@@ -4,8 +4,8 @@ require "os"
 
 --- compile all fennel into lua first using fennel application (should be installed via nixpkgs).
 --- this saves overhead of having to load fennel compiler into neovim each startup.
-local aot_compile_filename = "fennel_aot_compile.fnl"
-local command = "PACKAGE_PATH='" .. package.path .. "' fennel " .. os.getenv("HOME") .. "/.config/nvim/" .. aot_compile_filename
-os.execute(command)
+-- local aot_compile_filename = "fennel_aot_compile.fnl"
+-- local command = "PACKAGE_PATH='" .. package.path .. "' fennel " .. os.getenv("HOME") .. "/.config/nvim/" .. aot_compile_filename
+-- os.execute(command)
 require "util"
 require "plugins"
