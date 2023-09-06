ec3063d8a77b04b0d9e725b5bc673b68dd558440
jarrin-p <jarrin.scott@icloud.com>
2023-09-06 09:40:37 -0400

    fix aot compile.

--- managed/.config/nvim/pre-nvim-hook.sh
+++ managed/.config/nvim/pre-nvim-hook.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-conf="${HOME}/.config/nvim"
+conf="${HOME}/.config/nvim/"
 lrm_store_path=/tmp/fnl_compile
 last_recorded_mtime_path=${lrm_store_path}/last_recorded_mtime
 mkdir -p ${lrm_store_path}
 touch ${last_recorded_mtime_path}
 
 function get_most_recently_updated () {
-    find -E "${conf}/" \
+    find -E "${conf}" \
         -not -type d \
         -and \( -name '*.fnl' -or -name '*.lua' \) \
         | xargs -P0 -I{} stat --format='%Z' {} \
@@ -18,15 +18,15 @@ function get_most_recently_updated () {
 # todo: escape period.
 most_recently_updated=$(get_most_recently_updated)
 last_recorded_mtime=$(cat ${last_recorded_mtime_path})
-# echo "last recorded mtime path : ${last_recorded_mtime_path}"
-# echo "last recorded mtime      : ${last_recorded_mtime}"
-# echo "most recently updated    : ${most_recently_updated}"
+echo "last recorded mtime path : ${last_recorded_mtime_path}"
+echo "last recorded mtime      : ${last_recorded_mtime}"
+echo "most recently updated    : ${most_recently_updated}"
 
 if test "${most_recently_updated}" != "${last_recorded_mtime}" \
     || test -z "${last_recorded_mtime}"
 then
     echo "found changes"
-    find -E . -not -type d -and -name '*.fnl' \
+    find -E "${conf}" -not -type d -and -name '*.fnl' \
         |  {
             while read -r file_path
             do
