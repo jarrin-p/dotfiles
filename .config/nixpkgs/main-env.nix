53aa49d27469a60c32d25a446c561f4a078a2e57
jarrin-p <jarrin.scott@icloud.com>
2024-02-09 08:53:26 -0500

    usually used through `npm`.

--- .config/nixpkgs/main-env.nix
+++ .config/nixpkgs/main-env.nix
@@ -49,7 +49,6 @@ in
         nodePackages_latest.vscode-css-languageserver-bin
         nodePackages_latest.vscode-json-languageserver
         nodePackages_latest.vscode-html-languageserver-bin
-        nodePackages_latest.cdktf-cli
         pylint
         python310
         ranger
