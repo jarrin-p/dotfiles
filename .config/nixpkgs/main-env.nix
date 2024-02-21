d225762c2a5552700b664862e1a9b017c5c42257
jarrin-p <jarrin.scott@icloud.com>
2024-02-21 08:54:54 -0500

    fix lsps not loading since they return a list not a single pkg.

--- .config/nixpkgs/main-env.nix
+++ .config/nixpkgs/main-env.nix
@@ -7,59 +7,59 @@ let
 in
   with pkgs; buildEnv {
     name = "mainEnv";
-    paths = [
-        (gradle_7.override{ java = jdk11; })
-        (import ./packages/fennel.nix { pkgs = pkgs; })
-        (import ./packages/lsp.nix { pkgs = pkgs; })
-        (import ./packages/nvim.nix { pkgs = pkgs; })
+    paths = (import ./packages/lsp.nix { pkgs = pkgs; }) ++
+        [
+          (gradle_7.override{ java = jdk11; })
+          (import ./packages/fennel.nix { pkgs = pkgs; })
+          (import ./packages/nvim.nix { pkgs = pkgs; })
 
-        # version of rtorrent that doesn't break.
-        (import ./packages/rtorrent.nix {})
+          # version of rtorrent that doesn't break.
+          (import ./packages/rtorrent.nix {})
 
-        # version 1.15 of terraform.
-        (import ./packages/terraform.nix {})
+          # version 1.15 of terraform.
+          (import ./packages/terraform.nix {})
 
-        ansifilter # an actual savior.
-        bat
-        bitwarden-cli
-        black
-        cargo
-        code-minimap
-        coreutils-full
-        coursier
-        curl
-        direnv
-        ffmpeg
-        fd
-        fish
-        fnlfmt
-        fzf
-        gh
-        git
-        google-java-format
-        gnumake
-        jdk11
-        jq
-        moar
-        neovim-remote
-        nodejs_20
-        nodePackages_latest.typescript
-        nodePackages_latest.pyright
-        pylint
-        python310
-        ranger
-        readline
-        redis
-        rename
-        ripgrep
-        rustc
-        rust-analyzer
-        stow
-        tree
-        tmux
-        visidata
-        wget
-        yq
+          ansifilter # an actual savior.
+          bat
+          bitwarden-cli
+          black
+          cargo
+          code-minimap
+          coreutils-full
+          coursier
+          curl
+          direnv
+          ffmpeg
+          fd
+          fish
+          fnlfmt
+          fzf
+          gh
+          git
+          google-java-format
+          gnumake
+          jdk11
+          jq
+          moar
+          neovim-remote
+          nodejs_20
+          nodePackages_latest.typescript
+          nodePackages_latest.pyright
+          pylint
+          python310
+          ranger
+          readline
+          redis
+          rename
+          ripgrep
+          rustc
+          rust-analyzer
+          stow
+          tree
+          tmux
+          visidata
+          wget
+          yq
         ];
 }
 # broken packages.
