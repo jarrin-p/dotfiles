3ed8d5c66c7125b7f3b222c6cff2c8534203692f
jarrin-p <jarrin.scott@icloud.com>
2024-05-20 11:57:38 -0400

    use method from color-tool instead of pack.

--- .config/nvim/pack/statusline/util/component.lua
+++ .config/nvim/pack/statusline/util/component.lua
@@ -1,4 +1,4 @@
-local util = require 'pack.statusline.util.util'
+local get_colorscheme_as_hex = require 'utils.color-tool'["get-colorscheme-as-hex"]
 
 --- nvim highlight group wrapper that allows easier inline status text formatting.
 --- additionally, has defaults specified to keep the status/tab line uniform.
@@ -38,8 +38,8 @@ local M = {
     --- @param symbol string the symbol to use for transitioning.
     --- @return component transition the transition component.
     get_transition_to = function(self, component, identifier, symbol)
-        local from = util.get_colorscheme_as_hex(component.hl_name, identifier)
-        local to = util.get_colorscheme_as_hex(self.hl_name, identifier)
+        local from = get_colorscheme_as_hex(component.hl_name, identifier)
+        local to = get_colorscheme_as_hex(self.hl_name, identifier)
         local newComponentName = self.hl_name .. 'To' .. component.hl_name
         vim.api.nvim_set_hl(0, newComponentName, { bg = from, fg = to })
         return self:new{ hl_name = newComponentName, value = symbol }
@@ -53,8 +53,8 @@ local M = {
     --- @param highlight_group_override string the name of the highlight group you wish to override.
     --- @return component #a new component pointing at HighlightGroup that has reversed bg, fg of what this was called on.
     reversed = function(self, highlight_group_override)
-        local bg = util.get_colorscheme_as_hex(self.hl_name, 'background')
-        local fg = util.get_colorscheme_as_hex(self.hl_name, 'foreground')
+        local bg = get_colorscheme_as_hex(self.hl_name, 'background')
+        local fg = get_colorscheme_as_hex(self.hl_name, 'foreground')
         local newComponentName = highlight_group_override or self.hl_name .. 'Reversed'
         vim.api.nvim_set_hl(0, newComponentName, { bg = fg, fg = bg })
         return self:new{ hl_name = newComponentName }
