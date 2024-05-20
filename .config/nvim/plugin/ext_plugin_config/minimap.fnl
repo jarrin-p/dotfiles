242f3c713fc7678b4aa616198cd7d0d5cddfa772
jarrin-p <jarrin.scott@icloud.com>
2024-05-20 12:10:55 -0400

    update minimap to use fennel.

---
+++ .config/nvim/plugin/ext_plugin_config/minimap.fnl
@@ -0,0 +1,6 @@
+(set vim.g.minimap_width 15)
+(set vim.g.minimap_left 0)
+(set vim.g.minimap_git_colors 1)
+(set vim.g.minimap_highlight_search 1)
+(set vim.g.minimap_block_filetypes [:nofile :fugitive :fzf :help])
+{}
--- .config/nvim/plugin/ext_plugin_config/minimap.lua
+++
@@ -1,8 +0,0 @@
-vim.g.minimap_width = 15
-vim.g.minimap_left = 0
-vim.g.minimap_git_colors = 1
-vim.g.minimap_highlight_search = 1
-vim.g.minimap_block_filetypes = { 'nofile', 'fugitive', 'fzf', 'help' }
--- these break on a lot of buffers for some reason.
--- vim.g.minimap_auto_start = 1
--- vim.g.minimap_auto_start_win_enter = 1
