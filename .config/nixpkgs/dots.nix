c6334ae5956c524eed1bafb3d005a066b112c10d
jarrin-p <jarrin.scott@icloud.com>
2024-08-21 06:36:05 -0400

    remove unused shim util

--- .config/nixpkgs/dots.nix
+++ .config/nixpkgs/dots.nix
@@ -11,7 +11,6 @@ writeShellScriptBin "dots" ''
       dir          show the directory where the dotfiles are located.
       install      runs nix install for the main environment.
       refresh      alias for install, but more natural when updating.
-      setup_shell  add a shim to a shell that sources config accessors for used apps.
       uninstall    removes everything.
       usage        shows this.
   EOF
@@ -31,28 +30,6 @@ writeShellScriptBin "dots" ''
       fi
   }
 
-  setup_shell() {
-      test -z "$1" || test -z "$2" && {
-          echo "error. required arguments missing:"
-          echo "\$1 = bash | zsh | fish"
-          echo "\$2 = /path/to/dotx/config"
-          exit 1
-      }
-      case "$1" in
-          fish)
-              mkdir -p $HOME/.config/fish/conf.d
-              cat <<-EOF > $HOME/.config/fish/conf.d/dotx-config.fish
-  set -x DOTX_CONFIG_LOCATION "$2"
-  source \$DOTX_CONFIG_LOCATION/.config/fish/config.fish
-  EOF
-              ;;
-          *)
-              echo "unknown shell. aborting."
-              exit 1
-              ;;
-      esac
-  }
-
   handle() {
       case "$1" in
           install)
@@ -62,11 +39,6 @@ writeShellScriptBin "dots" ''
               ;;
           refresh)
               install_pkgs
-              ;;
-          setup_shell)
-              supported_shell=$(gum choose --header="shell to setup" "bash" "zsh" "fish")
-              supported_path=$(gum input --header="path to location of dotfiles" --value="$(pwd)")
-              setup_shell $supported_shell $supported_path
               ;;
           uninstall)
               nix-env --uninstall 'mainEnv'
@@ -79,7 +51,7 @@ writeShellScriptBin "dots" ''
 
   if test -z "$@"
   then
-      choice=$(gum choose --ordered "install" "setup_shell" "uninstall" "usage" "cancel")
+      choice=$(gum choose --ordered "install" "uninstall" "usage" "cancel")
       handle $choice
   else
       while ! test -z "$1"
