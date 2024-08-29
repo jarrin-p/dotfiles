61fee99536a5ab56bf747e2b6ccaaf2ba4c9f56b
jarrin-p <jarrin.scott@icloud.com>
2024-08-29 10:06:43 -0400

    remove unused dependency.

--- shell.nix
+++ shell.nix
@@ -1,5 +1,7 @@
 { pkgs ? import (builtins.fetchTarball { url = "https://api.github.com/repos/nixos/nixpkgs/tarball/24.05"; }) {} }:
 pkgs.mkShell {
   name = "lean-dev-env";
-  packages = [pkgs.lean4 pkgs.fennel];
+  packages = [
+    pkgs.fennel
+  ];
 }
